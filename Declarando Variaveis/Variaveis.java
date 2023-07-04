a153ca1c65319dcffc133df15469dba17684d075
danilocassiano <93688833+danilocassiano@users.noreply.github.com>
2023-07-04 17:48:36 -0300

    Aula de Declaração de Variavel
    Aula de Declaração de Variável.

---
+++ Declarando Variaveis/Variaveis.java
@@ -0,0 +1,15 @@
+public class Variaveis {
+    public static void main(String[] args){
+        int idade = 33;
+        String nome = "Danilo";
+        String nomeDoMeuCachorro = "Zoe";
+
+        idade = 25;
+
+        nome = "Micaela";
+
+        System.out.println("Idade = " + idade);
+        System.out.println("Nome = " + nome);
+        System.out.println("Minha cachorrinha se chama = " + nomeDoMeuCachorro);
+    }
+}
