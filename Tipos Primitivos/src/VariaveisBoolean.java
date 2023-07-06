55038ae714f0303410283841b4b5f5b138e45a99
danilocassiano <danilocassiano@hotmail.com>
2023-07-06 12:55:55 -0300

    create classe Boolean
    Criação de exemplo do Tipos Primitivos Boolean.

---
+++ Tipos Primitivos/src/VariaveisBoolean.java
@@ -0,0 +1,13 @@
+
+public class VariaveisBoolean {
+
+    public static void main(String[] args) {
+
+        boolean verdadeiro = true;
+
+        boolean falso = false;
+
+        System.out.println("O valor de Verdadeiro é: " + verdadeiro);
+        System.out.println("O valor de Falso é: " + falso);
+    }
+}
