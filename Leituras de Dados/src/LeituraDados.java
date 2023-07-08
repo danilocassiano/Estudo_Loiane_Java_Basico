18209ea7c3bb63a6d5e7c7240a69365b57ca9db1
danilocassiano <danilocassiano@hotmail.com>
2023-07-08 00:02:41 -0300

    "Estudos sobre Leitura de Dados"

---
+++ Leituras de Dados/src/LeituraDados.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class LeituraDados {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite sua Altura: ");
+        double altura = scan.nextDouble();
+        System.out.println("Sua altura é: " + altura);
+
+        // System.out.println("Digite o seu nome completo: ");
+        // String nomeCompleto = scan.ne;
+        // System.out.println("Seu nome completo é: " + nomeCompleto);
+        // System.out.println("Digite seu primeiro nome: ");
+        // String primeiroNome = scan.next();
+        // System.out.println("Seu primeiro nome é: " + primeiroNome);
+        // System.out.println("Digite sua Idade: ");
+        // int idade = scan.nextInt();
+        // System.out.println("Sua idade é: " + idade);
+    }
+}
