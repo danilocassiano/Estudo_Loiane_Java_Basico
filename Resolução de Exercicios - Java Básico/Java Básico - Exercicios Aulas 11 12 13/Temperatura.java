03e72cf0e6b0ff8d3ac78104e9aaf1d38d67f4e7
danilocassiano <danilocassiano@hotmail.com>
2023-07-15 18:06:25 -0300

    Update Exercicio
    Resolução do exercício 9.

---
+++ Resolução de Exercicios - Java Básico/Java Básico - Exercicios Aulas 11 12 13/Temperatura.java
@@ -0,0 +1,14 @@
+//EXERCICIO 9
+import java.util.Scanner;
+
+public class Temperatura {
+    public static void main(String[] args) {
+        Scanner temp = new Scanner(System.in);
+        System.out.println("Qual a temperatura em Farenheit?");
+        double farenheit = temp.nextDouble();
+
+        double celsius = (farenheit -32)/1.8;
+
+        System.out.println("A temperatura convertida Celsius é " + celsius);
+    }
+}
