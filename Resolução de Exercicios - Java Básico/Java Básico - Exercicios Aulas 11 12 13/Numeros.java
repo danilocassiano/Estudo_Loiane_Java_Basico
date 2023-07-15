54cb55da6fd9e23baf0ea21a1dc63a7405df71c8
danilocassiano <danilocassiano@hotmail.com>
2023-07-15 20:03:27 -0300

    Update Exercicio 11
    Solução do exercício 11.

---
+++ Resolução de Exercicios - Java Básico/Java Básico - Exercicios Aulas 11 12 13/Numeros.java
@@ -0,0 +1,24 @@
+//EXERCICIO 11
+import java.util.Scanner;
+import java.lang.Math;
+public class Numeros {
+    public static void main(String[] args) {
+        Scanner number = new Scanner(System.in);
+
+        System.out.println("Informe um número inteiro");
+        int primeiroNumero = number.nextInt();
+        System.out.println("Informe um segundo número inteiro");
+        int segundoNumero = number.nextInt();
+        System.out.println("Informe um terceiro número Real");
+        double terceiroNumero = number.nextDouble();
+
+        int produto = ( primeiroNumero * 2 ) * ( segundoNumero / 2 );
+        double soma = ( primeiroNumero * 3 ) + terceiroNumero;
+        double cubo = (double) Math.pow(terceiroNumero,3);
+
+        System.out.println("A solução da questão 'a' é: " + produto);
+        System.out.println("A solução da questão 'b' é: " + soma);
+        System.out.println("A solução da questão 'c' é: " + cubo);
+
+    }
+}
