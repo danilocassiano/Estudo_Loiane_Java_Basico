e98d357fbc20ae2a9b7af8caab481552c575d122
danilocassiano <danilocassiano@hotmail.com>
2023-07-17 12:50:47 -0300

    Update Exercício 2.
    Resolução Exerciíio 2.

---
+++ Resolução de Exercicios - Java Básico/Resolucao - Exercicios Aulas 14 15/src/TiposDeNumeros.java
@@ -0,0 +1,18 @@
+//EXERCÍCIO 2
+import java.util.Scanner;
+
+public class TiposDeNumeros {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite um número positivo ou negativo");
+        int numero = scan.nextInt();
+
+        if(numero >= 0){
+            System.out.print("O número é Positivo");
+        }else{
+            System.out.print("O número é Negativo");
+        }
+
+    }
+}
