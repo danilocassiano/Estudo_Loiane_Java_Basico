9b5cc43d2e79bab6bc91264fd762dc4109c42326
danilocassiano <danilocassiano@hotmail.com>
2023-07-21 18:52:02 -0300

    Aprendizado sobre o Loop "For";

---
+++ LoopFor/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+        System.out.println("______________________________________________");
+        for (int i = 5; i >= 0; i-- ){
+            System.out.println("i = " + i);
+        }
+        System.out.println("______________________________________________");
+        for (int i = 0, j = 10; i <= j ; i++, j--){
+            System.out.println("i tem valor: " + i + " e j tem valor: " + j);
+        }
+    }
+}
