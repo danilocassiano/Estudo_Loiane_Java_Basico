de22ad604f8335800334c552a55e3b3a4a76cfae
danilocassiano <danilocassiano@hotmail.com>
2023-07-26 22:19:31 -0300

    Organizando pasta de Loop;

--- LoopFor/src/EstudoFor/Main.java
+++ LoopFor/src/EstudoFor/Main.java
@@ -1,3 +1,4 @@
+package EstudoFor;
 
 public class Main {
     public static void main(String[] args) {
--- LoopWhile/src/EstudoWhile/LoopWhile.java
+++ LoopWhile/src/EstudoWhile/LoopWhile.java
@@ -1,3 +1,4 @@
+package EstudoWhile;
 
 public class LoopWhile {
     public static void main(String[] args) {
