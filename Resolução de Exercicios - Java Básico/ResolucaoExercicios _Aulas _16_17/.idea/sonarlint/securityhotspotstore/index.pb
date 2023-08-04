ddbcdded0259211fe8df2defafd77d65773094f7
Danilo Cassiano <93688833+danilocassiano@users.noreply.github.com>
2023-08-04 12:26:03 -0300

    Resolução do Exercicio 4;

---
+++ Resolução de Exercicios - Java Básico/ResolucaoExercicios _Aulas _16_17/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,7 @@
+
+H
+src/Exercicio1/Nota.java,8\7\8780ca872d7b6c62835561d2f20e299b03ad636b
+I
+src/Exercicio2/Login.java,9\3\93bde0a72609f0079de7ef3a65a4ecf686d26b0a
+M
+src/Exercicio3/Validacao.java,e\9\e941ca75cc84d9e98fde3b2932478cb751ffcc41
---
+++ Resolução de Exercicios - Java Básico/ResolucaoExercicios _Aulas _16_17/src/Exercicio4/TaxasPopulacionais.java
@@ -0,0 +1,30 @@
+package Exercicio4;
+
+public class TaxasPopulacionais {
+    public static void main(String[] args) {
+
+        int habitantesPaisA;
+        int habitantesPaisB;
+        double taxaPaisA;
+        double taxaPaisB;
+
+        habitantesPaisA = 80000;
+        taxaPaisA = 0.03;
+
+        habitantesPaisB = 200000;
+        taxaPaisB = 0.015;
+
+        int anos = 0;
+
+        while (habitantesPaisA < habitantesPaisB) {
+            habitantesPaisA += (int) (habitantesPaisA * taxaPaisA);
+            habitantesPaisB += (int) (habitantesPaisB * taxaPaisB);
+            anos++;
+        }
+
+        System.out.println("Após " + anos + " anos, a população do país A ultrapassará a população do país B.");
+        System.out.println("População do país A: " + habitantesPaisA);
+        System.out.println("População do país B: " + habitantesPaisB);
+
+    }
+}
