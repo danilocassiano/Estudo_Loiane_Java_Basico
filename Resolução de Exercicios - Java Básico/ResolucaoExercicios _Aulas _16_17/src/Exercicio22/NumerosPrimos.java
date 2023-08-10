ce47a0d0cb98a53ac6250922a5b04505f80eb515
Danilo Cassiano <93688833+danilocassiano@users.noreply.github.com>
2023-08-09 22:24:26 -0300

    Resolução do Exercicio 22

---
+++ Resolução de Exercicios - Java Básico/ResolucaoExercicios _Aulas _16_17/src/Exercicio22/NumerosPrimos.java
@@ -0,0 +1,41 @@
+package Exercicio22;
+
+import java.util.Scanner;
+
+public class NumerosPrimos {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        StringBuilder result = new StringBuilder();
+
+        System.out.println("Informe um número.");
+        int numero = scan.nextInt();
+        int i;
+
+        if (numero > 1) {
+            int qtdDivisivel = 0;
+            for (i = 2; i <= numero; i++) { // Começa a partir de 2
+                if (numero % i == 0) {
+                    qtdDivisivel++;
+                } else if (numero % i != 0) {
+                    for (i = 2; i <= numero; i++) {
+                        if (numero % i == 0) {
+                            result.append(i).append("-");
+                        }
+                    }
+                    if (result.length() > 0) {
+                        System.out.println("O número " + numero + " é divisivel por: " + result.substring(0, result.length() - 1));
+                    }
+                }
+            }
+
+            if (qtdDivisivel == 1) { // Verifica se tem exatamente 1 divisor além de 1 (ele mesmo)
+                System.out.println("Esse número é Primo.");
+            } else {
+                System.out.println("Esse número não é Primo.");
+            }
+
+        } else {
+            System.out.println("Esse número não é Primo.");
+        }
+    }
+}
