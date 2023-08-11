742e0e7efd66259c453c9c5e5bbb056e66d1dfdf
Danilo Cassiano <93688833+danilocassiano@users.noreply.github.com>
2023-08-11 13:27:35 -0300

    Resolução do Exercicio 24.

--- Resolução de Exercicios - Java Básico/ResolucaoExercicios _Aulas _16_17/.idea/sonarlint/issuestore/index.pb
+++ Resolução de Exercicios - Java Básico/ResolucaoExercicios _Aulas _16_17/.idea/sonarlint/issuestore/index.pb
@@ -7,7 +7,5 @@ I
 src/Exercicio2/Login.java,9\3\93bde0a72609f0079de7ef3a65a4ecf686d26b0a
 L
 src/Exercicio21/Numeros.java,4\0\400750e0855791d406927c0f2f9a120361c649c3
-R
-"src/Exercicio22/NumerosPrimos.java,7\5\75b3b126323db4ebf2acb25423abebebd8ab0321
 T
 $src/Exercicio5/TaxaPopulacional.java,1\4\14c8894435f98b6668243399cd58f47e0f2a2681
---
+++ Resolução de Exercicios - Java Básico/ResolucaoExercicios _Aulas _16_17/src/Exercicio24/MediaAritmetica.java
@@ -0,0 +1,33 @@
+package Exercicio24;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class MediaAritmetica {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Random random = new Random();
+        StringBuilder result = new StringBuilder();
+
+        System.out.println("Informe a quantidade de notas que o programa terá que fazer a Média Aritmetica:");
+        int qtdNotas = scan.nextInt();
+
+        int numeros;
+        int soma = 0;
+
+        for (int i = 1; i <= qtdNotas; i++) {
+            numeros = random.nextInt(10) + 1;
+            soma += numeros;
+            result.append(numeros).append("-");
+        }
+
+        if(result.length() > 0){
+            System.out.println("As notas geradas automaticamente é: " + result.substring(0, result.length() - 1));
+        }
+
+        double resultado = (double) soma / qtdNotas;
+
+        System.out.println("A média de " + qtdNotas + " notas aleatórias é: " + resultado);
+
+    }
+}
