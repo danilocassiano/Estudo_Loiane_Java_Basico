d9ddbcd622eebb51911e0bebb30fa000b4166f87
Danilo Cassiano <93688833+danilocassiano@users.noreply.github.com>
2023-08-11 12:13:30 -0300

    Update - Resolução do Exercicio 23. Adionando comentario explicando o código.

--- Resolução de Exercicios - Java Básico/ResolucaoExercicios _Aulas _16_17/src/Exercicio23/NumerosPrimos.java
+++ Resolução de Exercicios - Java Básico/ResolucaoExercicios _Aulas _16_17/src/Exercicio23/NumerosPrimos.java
@@ -15,8 +15,12 @@ public class NumerosPrimos {
         for (int i = 2; i <= n; i++) {
             boolean ehPrimo = true;
 
+            //No Próximo For, será verificado a Raiz Quadrada dos valores entre 2 e N. E depois será verificado
+            // a divisões até o Valor Quadrado do número. Por exemplo, se a Raiz Quadrada for 4, então a verificação
+            // será até 4. Se o resultado de alguma equação for 0 é porque existem outros numero divisiveis e esse numero
+            // não é Primo. Caso os resultados reste qualquer outro valor significa que ele é Primo.
             for (int j = 2; j <= Math.sqrt(i); j++) {
-                totalDivisoes++;
+                totalDivisoes++; // Aqui ele adiciona +1 toda vez que ele fizer uma divisão.
                 if (i % j == 0) {
                     ehPrimo = false;
                     break;
@@ -24,11 +28,12 @@ public class NumerosPrimos {
             }
 
             if (ehPrimo) {
-                quantidadePrimos++;
+                quantidadePrimos++; // Aqui ele adiciona +1 toda vez que um numero primo for encontrado dentro do intervalor de 1 a N.
                 System.out.println(i + " é um número primo.");
             }
         }
 
+        //Aqui será realizado a impressão dos resultados.
         System.out.println("Quantidade de números primos encontrados: " + quantidadePrimos);
         System.out.println("Número total de divisões realizadas: " + totalDivisoes);
 
