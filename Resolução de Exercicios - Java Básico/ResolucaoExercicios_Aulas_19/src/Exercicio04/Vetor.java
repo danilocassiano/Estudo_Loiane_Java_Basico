8060b7ea4d500a4aaf3ee3b11b82ebba059fc913
Danilo Cassiano <93688833+danilocassiano@users.noreply.github.com>
2023-09-12 20:29:36 -0300

    Resolução do Exercicio 04.

---
+++ Resolução de Exercicios - Java Básico/ResolucaoExercicios_Aulas_19/src/Exercicio04/Vetor.java
@@ -0,0 +1,35 @@
+package Exercicio04;
+
+public class Vetor {
+    public static void main(String[] args) {
+
+        int [] vetorA = new int[15];
+        double [] vetorB = new double[15];
+
+
+        vetorA [0] = 1;
+        vetorA [1] = 2;
+        vetorA [2] = 3;
+        vetorA [3] = 4;
+        vetorA [4] = 5;
+        vetorA [5] = 6;
+        vetorA [6] = 7;
+        vetorA [7] = 8;
+        vetorA [8] = 9;
+        vetorA [9] = 10;
+        vetorA [10] = 11;
+        vetorA [11] = 12;
+        vetorA [12] = 13;
+        vetorA [13] = 14;
+        vetorA [14] = 15;
+
+        for( int i = 0; i < vetorA.length; i++){
+            vetorB[i] = (double) Math.sqrt(vetorA[i]);
+        }
+
+        System.out.println("Vetor B:");
+        for (int i = 0; i < vetorB.length; i++){
+            System.out.println("B[" + i + "] = " + vetorB[i]);
+        }
+    }
+}
