82080908d97bcee9aaf15a532eed00405cff5a11
Danilo Cassiano <93688833+danilocassiano@users.noreply.github.com>
2023-09-13 13:18:52 -0300

    Resolução do Exercicio 12.

---
+++ Resolução de Exercicios - Java Básico/ResolucaoExercicios_Aulas_19/src/Exercicio12/Vetor.java
@@ -0,0 +1,27 @@
+package Exercicio12;
+
+public class Vetor {
+    public static void main(String[] args) {
+
+        int [] vetorA = new int[10];
+
+        vetorA [0] = 1;
+        vetorA [1] = 2;
+        vetorA [2] = 3;
+        vetorA [3] = 4;
+        vetorA [4] = 5;
+        vetorA [5] = 6;
+        vetorA [6] = 7;
+        vetorA [7] = 8;
+        vetorA [8] = 9;
+        vetorA [9] = 10;
+
+        int soma = 0;
+
+        for(int i = 0; i < vetorA.length; i++){
+            soma += vetorA[i];
+        }
+
+        System.out.println("O valor da soma de todos os valores do Vetor é: " + soma);
+    }
+}
