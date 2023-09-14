18ecfad53ab1874ee7fa607d13ffc2e7fcd1b6a0
Danilo Cassiano <93688833+danilocassiano@users.noreply.github.com>
2023-09-13 23:54:59 -0300

    Update do Exercicio 17.

--- Resolução de Exercicios - Java Básico/ResolucaoExercicios_Aulas_19/src/Exercicio17/Vetor.java
+++ Resolução de Exercicios - Java Básico/ResolucaoExercicios_Aulas_19/src/Exercicio17/Vetor.java
@@ -2,39 +2,26 @@ package Exercicio17;
 
 public class Vetor {
     public static void main(String[] args) {
-        int [] vetorA = new int[10];
+        int[] vetorA = new int[10];
 
-        vetorA [0] = 1;
-        vetorA [1] = 2;
-        vetorA [2] = 3;
-        vetorA [3] = 4;
-        vetorA [4] = 5;
-        vetorA [5] = 15;
-        vetorA [6] = 20;
-        vetorA [7] = 25;
-        vetorA [8] = 30;
-        vetorA [9] = 15;
+        vetorA[0] = 1;
+        vetorA[1] = 2;
+        vetorA[2] = 3;
+        vetorA[3] = 4;
+        vetorA[4] = 5;
+        vetorA[5] = 45;
+        vetorA[6] = 40;
+        vetorA[7] = 25;
+        vetorA[8] = 30;
+        vetorA[9] = 15;
 
-        int numeroMaior = Integer.MIN_VALUE;
-        int numeroMenor = Integer.MAX_VALUE;
-        int indiceMaior = -1;
-        int indiceMenor = -1;
-
-
-        for (int i = 0; i < vetorA.length; i++){
-            if(vetorA[i] > numeroMaior){
-                numeroMaior = vetorA[i];
-                indiceMaior = i;
-            }
-        }
+        int contador = 0;
 
         for(int i = 0; i < vetorA.length; i++){
-            if(vetorA[i] < numeroMenor){
-                numeroMenor = vetorA[i];
-                indiceMenor = i;
+            if(vetorA[i] > 35){
+                contador ++;
             }
         }
-        System.out.println("O maior número do Array é: " + numeroMaior + " e o indice dele é: " + indiceMaior);
-        System.out.println("O menor número do Array é: " + numeroMenor + " e o indice dele é: " + indiceMenor);
+        System.out.println("Existem " + contador + " Pessoas com idade acima de 35 anos.");
     }
 }
