a37b8f2af2a967d1eaeadd1a14fe734b325806b7
Danilo Cassiano <93688833+danilocassiano@users.noreply.github.com>
2023-09-15 21:21:07 -0300

    Resolução do Exercicio 01.

---
+++ Resolução de Exercicios - Java Básico/ResolucaoExercicios_Aulas_20/src/Exercicio01/Matriz.java
@@ -0,0 +1,54 @@
+package Exercicio01;
+
+import java.text.Format;
+
+public class Matriz {
+    public static void main(String[] args) {
+
+        int [] [] matriz = new int[4][4];
+
+        matriz[0][0] = 2;
+        matriz[0][1] = 4;
+        matriz[0][2] = 5;
+        matriz[0][3] = 9;
+
+        matriz[1][0] = 3;
+        matriz[1][1] = 8;
+        matriz[1][2] = 1;
+        matriz[1][3] = 5;
+
+        matriz[2][0] = 4;
+        matriz[2][1] = 8;
+        matriz[2][2] = 2;
+        matriz[2][3] = 7;
+
+        matriz[3][0] = 6;
+        matriz[3][1] = 4;
+        matriz[3][2] = 1;
+        matriz[3][3] = 3;
+
+        int minimo = matriz[0][0];
+        int maximo = matriz[0][0];
+
+        for(int i = 0; i < matriz.length; i++){
+            for(int j = 0; j < matriz[i].length; j++){
+                int valor = matriz [i][j];
+
+                if(valor < minimo){
+                    minimo = valor;
+                }
+
+                if(valor > maximo){
+                    maximo = valor;
+                }
+            }
+        }
+
+        System.out.println("O valor mínimo na matriz é: " + minimo);
+        System.out.println("O valor máximo na matriz é: " + maximo);
+
+
+    }
+}
+
+
