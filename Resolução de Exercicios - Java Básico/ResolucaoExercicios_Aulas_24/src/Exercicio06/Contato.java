9ad22af11ad1b12c80526e5f3ff08a059281986b
Danilo Cassiano <93688833+danilocassiano@users.noreply.github.com>
2023-10-02 22:25:32 -0300

    Resolução do Exercicio 06.

---
+++ Resolução de Exercicios - Java Básico/ResolucaoExercicios_Aulas_24/src/Exercicio06/Contato.java
@@ -0,0 +1,8 @@
+package Exercicio06;
+
+public class Contato {
+    String nome;
+    String email;
+    String endereco;
+    String[] numeros;
+}
---
+++ Resolução de Exercicios - Java Básico/ResolucaoExercicios_Aulas_24/src/Exercicio06/ExibirContato.java
@@ -0,0 +1,18 @@
+package Exercicio06;
+
+public class ExibirContato
+{
+    public static void main(String[] args) {
+        Contato contato = new Contato();
+        contato.nome = "Danilo";
+        contato.email = "danilocassiano@hotmai.com";
+        contato.endereco = "Rua rua, 11";
+        contato.numeros = new String[5];
+        contato.numeros[0] = "9999-9999";
+        contato.numeros[1] = "9999-9998";
+        contato.numeros[2] = "9999-9997";
+        contato.numeros[3] = "9999-9996";
+        contato.numeros[4] = "9999-9995";
+
+    }
+}
