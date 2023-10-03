824f12fc1fac33a03874d8bdc98e033303b22383
Danilo Cassiano <93688833+danilocassiano@users.noreply.github.com>
2023-10-02 22:04:52 -0300

    Resolução do Exercicio 03.

---
+++ Resolução de Exercicios - Java Básico/ResolucaoExercicios_Aulas_24/src/Exercicio03/ExibirLivraria.java
@@ -0,0 +1,15 @@
+package Exercicio03;
+
+import Exercicio02.Livros;
+
+public class ExibirLivraria {
+    public static void main(String[] args) {
+        LivroDeLivraria Livro = new LivroDeLivraria();
+
+        Livro.nome = "Harry Potter";
+        Livro.autor = "Katerine";
+        Livro.anoLancamento = 2015;
+        Livro.qtdPaginas = 500;
+        Livro.preco = 250;
+    }
+}
---
+++ Resolução de Exercicios - Java Básico/ResolucaoExercicios_Aulas_24/src/Exercicio03/LivroDeLivraria.java
@@ -0,0 +1,9 @@
+package Exercicio03;
+
+public class LivroDeLivraria {
+    String nome;
+    String autor;
+    int qtdPaginas;
+    int anoLancamento;
+    double preco;
+}
