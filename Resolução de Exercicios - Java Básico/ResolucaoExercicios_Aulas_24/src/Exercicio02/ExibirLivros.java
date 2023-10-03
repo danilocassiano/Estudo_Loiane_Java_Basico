5dc367eedac72680cc7ed35a2aa1cdbe2f42f220
Danilo Cassiano <93688833+danilocassiano@users.noreply.github.com>
2023-10-02 22:00:24 -0300

    Resolução do Exercicio 02.

---
+++ Resolução de Exercicios - Java Básico/ResolucaoExercicios_Aulas_24/src/Exercicio02/ExibirLivros.java
@@ -0,0 +1,13 @@
+package Exercicio02;
+
+public class ExibirLivros
+{
+    public static void main(String[] args) {
+        Livros Livro = new Livros();
+
+        Livro.nome = "Harry Potter";
+        Livro.autor = "Katerine";
+        Livro.anoLancamento = 2015;
+        Livro.qtdPaginas = 500;
+    }
+}
---
+++ Resolução de Exercicios - Java Básico/ResolucaoExercicios_Aulas_24/src/Exercicio02/Livros.java
@@ -0,0 +1,8 @@
+package Exercicio02;
+
+public class Livros {
+    String nome;
+    String autor;
+    int qtdPaginas;
+    int anoLancamento;
+}
